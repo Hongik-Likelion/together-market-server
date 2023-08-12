0aaa61a4b94484c5c6f72dc06659622f6f6b5c7c
yongun2 <djkye0001@naver.com>
2023-08-13 02:26:26 +0900

    [FEAT] 상품 유형 조회 기능

--- products/views.py
+++ products/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from products.models import Product
+
 
 # Create your views here.
+@api_view(["GET"])
+def products_list_view(request):
+    product_list = Product.objects.all()
+
+    return_data = []
+
+    for product in product_list:
+        return_data.append(
+            {
+                "product_id": product.product_id,
+                "product_type": product.product_type,
+            }
+        )
+
+    return Response(data=return_data, status=status.HTTP_200_OK)
