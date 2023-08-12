71de78ee634b308f99edf43049d5a5637a72e10f
HyunWoo <kimjd766@gmail.com>
2023-08-12 12:29:39 +0900

    feat:시장 목록 조회 api 추가

--- market/views.py
+++ market/views.py
@@ -1,3 +1,12 @@
+import rest_framework.generics
 from django.shortcuts import render
 
+from market.models import Market
+from market.serializers import MarketSerializer
+
+
 # Create your views here.
+class MarketListAPIView(rest_framework.generics.ListAPIView):
+    queryset = Market.objects.all()
+    serializer_class = MarketSerializer
+
